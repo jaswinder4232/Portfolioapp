2d1e4ade5a24031cedd6db17576c963e188f1f1d
jaswinder4232 <jaswindersingh4232@gmail.com>
2026-02-17 17:02:11 +0530

    clearity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import MicrosoftClarity from "./components/MicrosoftClarity";
 import Visitor from "./components/visitor";
 import Script from "next/script";
 import ScrollTracker from "./components/ScrollTracker";
@@ -41,18 +42,7 @@ export default function RootLayout({ children }) {
             });
           `}
         </Script>
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-        >
-          {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
-  `}
-        </Script>
+        <MicrosoftClarity />
 
       </head>
       <body
